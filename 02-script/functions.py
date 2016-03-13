f2383f389ebdb6e7817422c596c58729f881003f
Yanael Barbier <ybarbier@student.42.fr>
2016-03-13 20:52:02 +0100

    Add info function

--- 02-script/functions.py
+++ 02-script/functions.py
@@ -20,6 +20,32 @@ def pcn(debugger, command, result, dct):
 	for i in range(0, int(args[0])):
 		debugger.HandleCommand("process continue")
 
+def colorR(debugger, command, result, dct):
+	debugger.HandleCommand('script print "\033[31m"')
+
+def colorG(debugger, command, result, dct):
+	debugger.HandleCommand('script print "\033[32m"')
+
+def colorB(debugger, command, result, dct):
+	debugger.HandleCommand('script print "\033[34m"')
+
+def colorW(debugger, command, result, dct):
+	debugger.HandleCommand('script print "\033[39m"')
+
+def colorT(debugger, command, result, dct):
+	debugger.HandleCommand('script print "\033[36m"')
+
+def info(debugger, command, result, dct):
+	colorR(debugger, command, result, dct)
+	debugger.HandleCommand("target list")
+	colorG(debugger, command, result, dct)
+	debugger.HandleCommand("disas")
+	colorB(debugger, command, result, dct)
+	debugger.HandleCommand("register read --all")
+	colorT(debugger, command, result, dct)
+	debugger.HandleCommand("frame select")
+	colorW(debugger, command, result, dct)
+
 def pc2(debugger, command, result, dct):
 	debugger.HandleCommand("process continue")
 	debugger.HandleCommand("process continue")
@@ -35,3 +61,9 @@ def __lldb_init_module(debugger, internal_dict):
 	debugger.HandleCommand("command script add -f functions.pc2 pc2")
 	debugger.HandleCommand("command script add -f functions.pc3 pc3")
 	debugger.HandleCommand("command script add -f functions.pcn pcn")
+	debugger.HandleCommand("command script add -f functions.colorR colorR")
+	debugger.HandleCommand("command script add -f functions.colorG colorG")
+	debugger.HandleCommand("command script add -f functions.colorB colorB")
+	debugger.HandleCommand("command script add -f functions.colorW colorW")
+	debugger.HandleCommand("command script add -f functions.colorT colorT")
+	debugger.HandleCommand("command script add -f functions.info info")
