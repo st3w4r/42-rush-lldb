67dab0397d828afd394b2cedb5622b03b2f3f316
Yanael Barbier <ybarbier@student.42.fr>
2016-03-13 20:18:38 +0100

    Function process in for

--- 02-script/functions.py
+++ 02-script/functions.py
@@ -6,6 +6,20 @@ def pcfv(debugger, command, result, dct):
 	debugger.HandleCommand("process continue")
 	debugger.HandleCommand("frame variables")
 
+def pcn(debugger, command, result, dct):
+	args = command.split(" ")
+	if len(args) is not 1:
+		print "Usage: one argument, numbers of process repetition"
+		return
+	try:
+		val = int(args[0])
+	except ValueError:
+		print "Usage: one argument, numbers of process repetition"
+		return
+
+	for i in range(0, int(args[0])):
+		debugger.HandleCommand("process continue")
+
 def pc2(debugger, command, result, dct):
 	debugger.HandleCommand("process continue")
 	debugger.HandleCommand("process continue")
@@ -20,3 +34,4 @@ def __lldb_init_module(debugger, internal_dict):
 	debugger.HandleCommand("command script add -f functions.pcfv pcfv")
 	debugger.HandleCommand("command script add -f functions.pc2 pc2")
 	debugger.HandleCommand("command script add -f functions.pc3 pc3")
+	debugger.HandleCommand("command script add -f functions.pcn pcn")
