e21f704749ec9d598424a019caaca17978181211
Catherine MADINIER <cmadinie@e2r4p13.42.fr>
2016-03-13 17:22:56 +0100

    nope

--- 02-script/reverse.py
+++ 02-script/reverse.py
@@ -2,7 +2,10 @@ import lldb
 
 def reverse(debugger, args, result, internal_dict):
 	target = debugger.GetSelectedTarget()
-	print "FT_"+str(target)[::-1]
+	if not target:
+		print "No target there!"
+	else:
+		print "FT_"+str(target)[::-1]
 
 def __lldb_init_module(debugger, internal_dict):
 	debugger.HandleCommand('command script add -f reverse.reverse reverse')
